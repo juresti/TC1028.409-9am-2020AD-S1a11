7e6ae244183be0e7c4d9eeae639e940f9563c781
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2020-09-02 08:46:57 -0500

    Update fib.py

--- 31Ago20/fib.py
+++ 31Ago20/fib.py
@@ -3,7 +3,7 @@ def sumaFibonacci():
     pen = 0
     res = ult + pen
     bueno = res
-    while (res < 1000):
+    while (res < 4000000):
         pen = ult
         ult = res
         res = ult + pen
